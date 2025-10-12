b429d13aeb87f8d3a55f3e0ff9f9e9753639e174
PierreMrt <pierre.merlet@proton.me>
2025-10-12 20:07:22 +0200

    added feature engineering

---
+++ scripts/feature_engineering.py
@@ -0,0 +1,23 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, OneHotEncoder
+
+def get_feature_transformer():
+    """
+    Feature engineering pour pipeline sklearn :
+      - 'ec (cm3)' : StandardScaler (ou MinMaxScaler)
+      - 'ep (KW)' : StandardScaler (ou MinMaxScaler)
+      - 'm (kg)' : StandardScaler
+      - 'Fuel consumption' : RobustScaler
+      - 'age_months' : MinMaxScaler (ou StandardScaler)
+      - 'Ft' et 'Mk' : encodage OneHotEncoder (handle_unknown='ignore')
+    """
+    return ColumnTransformer([
+        ('ec', StandardScaler(), ['ec (cm3)']),
+        ('ep', StandardScaler(), ['ep (KW)']),
+        ('m', StandardScaler(), ['m (kg)']),
+        ('fuel', RobustScaler(), ['Fuel consumption']),
+        ('age', MinMaxScaler(), ['age_months']),
+        ('Ft', OneHotEncoder(handle_unknown='ignore'), ['Ft']),
+        ('Mk', OneHotEncoder(handle_unknown='ignore'), ['Mk']),
+        ('Country', OneHotEncoder(handle_unknown='ignore'), ['Country'])
+    ])
